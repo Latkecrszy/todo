83bb06fd31e197a162d0e220d7d4dbddccbc2ea0
Seth Raphael <setharaphael7@gmail.com>
2020-11-17 14:42:37 -0800

    made the api

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, request
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/todo')
-def todo():
-    data = [{"key1": "value1", "key2": "value2"}]
-    res = make_response(jsonify(data))
-    res.mimetype = 'application/json'
-    return res
+@app.route('/todo/<id>', methods=["GET"])
+@app.route('/todo/', methods=["POST"])
+def todo(id=None):
+    with open("todos.json") as f:
+        todos = json.load(f)
+    if request.method == "GET":
+        if len(todos) <= int(id):
+            id = len(todos)
+        data = todos[id]
+        res = make_response(jsonify(data))
+        res.mimetype = 'application/json'
+        return res
+    elif request.method == "POST":
+        payload = request.get_json()
+        todos.append({"id": payload["id"], "title": payload["title"], "userId": payload["userId"], "completed": payload["completed"]})
+    with open("todos.json", "w") as f:
+        json.dump(todos, f, indent=4)
 
 
 if __name__ == '__main__':
