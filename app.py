8c7e45da1d64d2d4befa9d6a7676655d8813fa37
Seth Raphael <setharaphael7@gmail.com>
2020-11-17 15:09:54 -0800

    trying it again

--- app.py
+++ app.py
@@ -23,11 +23,13 @@ def todo(id=None):
     elif request.method == "POST":
         payload = request.get_json()
         todos.append({"title": payload.get("title", None), "userId": payload.get("userId", None), "completed": payload.get("completed", None)})
+        print(todos)
         data = {"title": payload.get("title", None), "userId": payload.get("userId", None),
                 "completed": payload.get("completed", None)}
+        with open("todos.json", "w") as f:
+            json.dump(todos, f, indent=4)
         return jsonify(data), 200
-    with open("todos.json", "w") as f:
-        json.dump(todos, f, indent=4)
+
 
 
 
