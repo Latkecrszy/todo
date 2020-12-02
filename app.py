50d418ace42188a7209274ca90f4a7771359879f
Seth Raphael <setharaphael7@gmail.com>
2020-12-01 20:55:59 -0800

    fixed the get and post methods

--- app.py
+++ app.py
@@ -1,34 +1,30 @@
 from flask import Flask, make_response, jsonify, request
 import json
+import os
 
 app = Flask(__name__)
 
 
-@app.route('/todo/<id>', methods=["GET"])
+@app.route('/todo/<int:id>', methods=["GET"])
 @app.route('/todo/', methods=["POST"])
 def todo(id=None):
     with open("todos.json") as f:
         todos = json.load(f)
     if request.method == "GET":
-        if len(todos) <= int(id):
-            id = len(todos)
-        else:
-            id = int(id)
-            id -= 1
-        print(todos)
-        print(id)
-        if int(id) < 0:
-            id = 0
-        data = todos[int(id)]
+        data = [todos[str(todo)] for todo in todos.keys() if int(todo) == id]
+        data = data[0]
         res = make_response(jsonify(data))
         res.mimetype = 'application/json'
-        return res
+
+        return res, 200
     elif request.method == "POST":
         payload = request.get_json()
-        todos.append({"title": payload.get("title", None), "userId": payload.get("userId", None), "completed": payload.get("completed", None)})
-        print(todos)
+        intKeys = [int(key) for key in todos]
+        maxKey = max(intKeys)
+        newKey = maxKey + 1
+        todos[str(newKey)] = {"title": payload.get("title", None), "userId": payload.get("userId", None), "completed": payload.get("completed", None)}
         data = {"title": payload.get("title", None), "userId": payload.get("userId", None),
-                "completed": payload.get("completed", None)}
+                "completed": payload.get("completed", None), "todoId": newKey}
         with open("todos.json", "w") as f:
             json.dump(todos, f, indent=4)
         return jsonify(data), 200
@@ -37,4 +33,5 @@ def todo(id=None):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
