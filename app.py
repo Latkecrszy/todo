69af88141c7fec96ae5b855e74bc49668c199ccc
Seth Raphael <setharaphael7@gmail.com>
2021-01-16 17:22:37 -0800

    fixed some lil buggies

--- app.py
+++ app.py
@@ -14,23 +14,26 @@ cors = CORS(app, resources={r'/todo/*': {"origins": ["https://cdpn.io"]}})
 def todo(id=None):
     mongo = PyMongo(app)
     if request.method == "GET":
-        data = mongo.db.todo.find_one({"id": str(id)})
+        data = mongo.db.todo.find_one({"todoId": id})
         data = {key: value for key, value in data.items() if key != "_id"}
         res = make_response(jsonify(data))
         res.mimetype = 'application/json'
         return res, 200
     elif request.method == "POST":
         payload = request.get_json()
+        if mongo.db.counters.find_one({"_id": "todo"}) is None:
+            mongo.db.counters.insert_one({"_id": "todo", "num": 0})
         id = mongo.db.counters.find_one_and_update({"_id": "todo"}, {"$inc": {"num": 1}}, return_document=ReturnDocument.AFTER)['num']
         data = {"title": payload.get("title", None), "userId": payload.get("userId", None),
                 "completed": payload.get("completed", None), "todoId": id}
         mongo.db.todo.insert_one(data)
+        data = {key: value for key, value in data.items() if key != "_id"}
         res = make_response(jsonify(data))
         res.mimetype = 'application/json'
         return res, 200
     elif request.method == "PUT":
         payload = request.get_json()
-        mongo.db.todo.find_one_and_replace({"todoId": str(id)}, {"title": payload.get("title", None), "userId": payload.get("userId", None),
+        mongo.db.todo.find_one_and_replace({"todoId": id}, {"title": payload.get("title", None), "userId": payload.get("userId", None),
                 "completed": payload.get("completed", None), "todoId": id})
         data = {"title": payload.get("title", None), "userId": payload.get("userId", None),
                 "completed": payload.get("completed", None), "todoId": id}
@@ -38,12 +41,6 @@ def todo(id=None):
         res.mimetype = 'application/json'
         return res, 200
 
-
-@app.route("/mongo")
-def mongomongomongo():
-    mongo = PyMongo(app)
-    mongo.db.todo.insert_one({"test": "thing"})
-    return "nothing :)", 200
 
 
 app.register_error_handler(405, lambda e: 'Error: Method Not Allowed')
@@ -56,4 +53,4 @@ app.register_error_handler(400, lambda e: 'Error: Bad Request')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=True)
+    app.run(host='0.0.0.0', port=port, use_reloader=True)
